60abbcdbaf491bd7bcd6efb111e9f6ba47d422b6
icaro-lauria <icaro.lauria@gmail.com>
2025-09-05 09:32:34 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Gerente gerente = new Gerente("Juan", 010101, 4200.69, 420);
-		System.out.println(gerente.calcularSalarioFinal());
+		System.out.println("O gerente "+gerente.getNome()
+			+ " recebeu: R$"+gerente.calcularSalarioFinal()+" neste mês.");
 		Vendedor vendedor = new Vendedor("Eric", 102125, 3333.33, 80);
-		System.out.println(vendedor.calcularSalarioFinal());
+		System.out.println("O vendedor "+vendedor.getNome()
+			+ " recebeu: R$"+vendedor.calcularSalarioFinal()+" neste mês.");
 	}
 
 }
